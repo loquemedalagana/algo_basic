9af0532add334f1710bed30026ea8eed00eb8e3a
loquemedalagana <nadamasquetu012@gmail.com>
2020-10-08 11:57:08 +0900

    add lowdash

--- boj_17478.js
+++ boj_17478.js
@@ -5,7 +5,6 @@ const getReadLine = () => {
         output: process.stdout
     })
 }
-
 const rl = getReadLine();
 const question = `"재귀함수가 뭔가요?"`;
 const ansOne = `"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.`;
@@ -14,38 +13,27 @@ const ansThree = `그의 답은 대부분 옳았다고 하네. 그런데 어느 
 
 const ansBase = `"재귀함수는 자기 자신을 호출하는 함수라네"`;
 const ansLast = `라고 답변하였지.`;
+const lowDash = '____';
 
 const solve = () => {
-    console.log('어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.');
-    
-    const lodash = n => {
-        if(n === 0) return '';
-        let ret = new Array(n*4).fill('_'); 
-        return ret.join('');
-    }
-    
-    const go = loop => {        
-        
-        console.log(lodash(loop) + question);
-
+    console.log('어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.');    
+    const go = loop => {                
+        console.log(lowDash.repeat(loop) + question);
         if(loop < N){
-            console.log(lodash(loop) + ansOne);
-            console.log(lodash(loop) + ansTwo);
-            console.log(lodash(loop) + ansThree);
+            console.log(lowDash.repeat(loop) + ansOne);
+            console.log(lowDash.repeat(loop) + ansTwo);
+            console.log(lowDash.repeat(loop) + ansThree);
             go(loop+1);
         }
         else if(loop === N){
-            console.log(lodash(loop) + ansBase);
+            console.log(lowDash.repeat(loop) + ansBase);
         }
-
-        console.log(lodash(loop) + ansLast);
+        console.log(lowDash.repeat(loop) + ansLast);
     }
 
     go(0);
 }
-
 let N;
-
 const start = rl => {
     rl.on('line', line => {
         N = parseInt(line);
@@ -54,5 +42,4 @@ const start = rl => {
         process.exit();
     })
 }
-
 start(rl);
